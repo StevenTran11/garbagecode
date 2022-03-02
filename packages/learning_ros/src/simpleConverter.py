756765a3fb06e1c3c9589e1d256e4fe6e1f1a49d
Steven Tran <Steven_Tran1@student.uml.edu>
2022-03-02 13:00:01 -0500

    hw5.3

--- packages/learning_ros/src/simpleConverter.py
+++ packages/learning_ros/src/simpleConverter.py
@@ -14,22 +14,22 @@ class simpleConverter:
 		self.WOR = Vector2D()
 		self.sensorAngle = 180 * math.pi / 180
 		self.robotAngle = 135 * math.pi / 180
-		self.RPS = np.matrix([-1,0])
-		self.RTS = np.matrix([[math.cos(self.sensorAngle),-math.sin(self.sensorAngle),self.RPS[0,0]], [math.sin(self.sensorAngle), math.cos(self.sensorAngle), self.RPS[0,1]],[0,0,1]])
-		self.WPR = np.matrix([3,2])
-		self.WTR = np.matrix([[math.cos(self.robotAngle), -math.sin(self.robotAngle),self.WPR[0,0]],[math.sin(self.robotAngle),-math.cos(self.robotAngle),self.WPR[0,1]],[0,0,1]])
-		self.SP = np.matrix([[0],[0],[1]])
-		self.WP = np.matrix([[0],[0],[1]])
-		self.sensorPos = ([[0],[0],[1]])
+		self.RPS = np.array([-1,0])
+		self.RTS = np.array([[math.cos(self.sensorAngle),-math.sin(self.sensorAngle),self.RPS[0]], [math.sin(self.sensorAngle), math.cos(self.sensorAngle), self.RPS[1]], [0, 0, 1]])
+		self.WPR = np.array([3,2])
+		self.WTR = np.array([[math.cos(self.robotAngle), -math.sin(self.robotAngle),self.WPR[0]],[math.sin(self.robotAngle), -math.cos(self.robotAngle),self.WPR[1]], [0,0,1]])
+		self.SP = np.array([[0],[0],[1]])
+		self.WP = np.array([[0],[0],[1]])
+		self.sensorPos = np.array([[0],[0],[1]])
 
 	def callback(self, msg):
 		self.sensorPos[0] = msg.x
 		self.sensorPos[1] = msg.y
-		self.SP = self.RTS * self.sensorPos
+		self.SP = np.dot(self.RTS,self.sensorPos)
 		self.ROB.x = self.SP[0]
 		self.ROB.y = self.SP[1]
 		self.robot.publish(self.ROB)
-		self.WP = self.WTR * self.SP
+		self.WP = np.dot(self.WTR,self.SP)
 		self.WOR.x = self.WP[0]
 		self.WOR.y = self.WP[1]
 		self.world.publish(self.WOR)
